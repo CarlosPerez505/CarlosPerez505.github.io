91625ba084c8834de9eaa9a6b739f025d0599c50
Carlos Perez <cp3develops@gmail.com>
2024-10-19 10:10:16 -0600

    configuring git to work with wsl2

---
+++ portfolio-backend/Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="carlos"
+
+ENTRYPOINT ["top", "-b"]
---
+++ portfolio-backend/src/app_tsts.ts
@@ -0,0 +1,8 @@
+import { handler } from "./app.ts";
+import { serve } from "https://deno.land/std@0.153.0/http/server.ts";
+
+Deno.test("server starts successfully", async () => {
+    console.log("Starting test server on http://localhost:8001");
+    await serve(handler, { port: 8001 });
+});
+
