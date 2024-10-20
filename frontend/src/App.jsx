4b4f66034cfa57a7cb392fb72ff39be691d95331
carlos <carlosperez505@gmail.com>
2024-10-19 23:30:58 -0600

    Fixed component spacing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Portfolio from './pages/Portfolio';
 import BlogList from './components/BlogList.jsx';
-import BlogPost from './components/BlogPost.jsx'; // Import the BlogPost component
+import BlogPost from './components/BlogPost.jsx';
 import Footer from './components/Footer';
 
 function App() {
@@ -20,15 +20,24 @@ function App() {
     return (
         <Router>
             <div className={`min-h-screen flex flex-col ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-gray-100 text-black'}`}>
+                {/* Navbar Section */}
                 <NavBar theme={theme} toggleTheme={toggleTheme} />
-                <div className="flex-grow">
+
+                {/* Main Content Section */}
+                <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 flex-grow">
                     <Routes>
+                        {/* Route for Portfolio Page */}
                         <Route path="/" element={<Portfolio theme={theme} />} />
+
+                        {/* Route for Blog List Page */}
                         <Route path="/blog" element={<BlogList theme={theme} />} />
-                        {/* Add route for individual blog posts */}
-                        <Route path="/blog/:id" element={<BlogPost />} /> {/* This route handles the blog post by ID */}
+
+                        {/* Route for Individual Blog Post */}
+                        <Route path="/blog/:id" element={<BlogPost />} />
                     </Routes>
                 </div>
+
+                {/* Footer Section */}
                 <Footer />
             </div>
         </Router>
